9b59c85b1c4ef0021c9cd64628e8388141551f90
Andreu <andreu.sck@gmail.com>
2020-08-19 03:04:16 +0200

    socket1

--- server.js
+++ server.js
@@ -2,8 +2,27 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+var io = require('socket.io')(http);
+
+
+// app.get('/', (req, res) => {
+//     res.send('Deployed! asdjasdkjasldk');
+// });
 app.get('/', (req, res) => {
-    res.send('Deployed! asdjasdkjasldk');
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+        console.log('message: ' + msg);
+
+    });
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
 });
 
 app.listen(port, () => {
