21ae7c755acf7a13f780bcf3fa9a37a14e508cc9
Andreu <andreu.sck@gmail.com>
2020-08-19 02:58:01 +0200

    aa1

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Deployed!');
+    res.send('Deployed! asdjasdkjasldk');
 });
 
 app.listen(port, () => {
